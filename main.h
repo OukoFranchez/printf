bd14b44253e0447231d058da9410c197857d3fe9
Bulimo <ubaga91@gmail.com>
2023-04-22 16:49:47 +0000

    Updated prototypes in main.h

--- main.h
+++ main.h
@@ -1,12 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list parg);
+int print_int(va_list parg);
+int print_string(va_list parg);
+int print_rev(char *s);
+int _strlen(char *s);
+int switch_case(char c, va_list parg);
 
-
-
-#endif /* MAIN_H */
+#endif
