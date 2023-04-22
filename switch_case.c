a9bee0ad1b3ebe8853487bfddc3a2b3e1ce6f898
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-04-22 19:56:37 +0300

    added switch_case function

---
+++ switch_case.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  * switch_case - switches cases of formats
+  * @c: character format
+  * @parg: pointer to variable argument list
+  * Return: int
+  */
+int switch_case(char c, va_list parg)
+{
+	int count = 0;
+
+	switch (c)
+	{
+		case 'c':
+			count += print_char(parg);
+			break;
+		case 's':
+			count += print_string(parg);
+			break;
+		case '%':
+			putchar('%');
+			count++;
+			break;
+		case 'd':
+		case 'i':
+			count += print_int(parg);
+			break;
+		default:
+			putchar('%');
+			putchar(c);
+			count += 2;
+			break;
+	}
+	return (count);
+}
+
